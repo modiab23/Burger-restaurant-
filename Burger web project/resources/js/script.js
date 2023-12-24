faf8ef5e71e0d34b5300b2575d1f02b27b3b3e42
Mohamed Diab <154678467+modiab23@users.noreply.github.com>
2023-12-24 10:00:40 +0200

    Add files via upload

---
+++ Burger web project/resources/js/script.js
@@ -0,0 +1,6 @@
+function search() {
+    let search = document.querySelector('.search');
+    let btn = document.querySelector('#btn');
+    btn.classList.toggle('fa-times');
+    search.classList.toggle('active');
+}
